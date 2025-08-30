4ab1c82178ca4b6bcdf5cd6cc3d48d5b4df3b97d
Juanjo Romero <juanolas73@gmail.com>
2025-08-30 18:22:34 +0200

    Use gpt2 III

--- utils/gentxt.py
+++ utils/gentxt.py
@@ -6,9 +6,6 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 import textwrap
 
-#def generateTXT():
-#    ss 
-
 def generateTXTconcat(model, tokenizer, prompt, num_words=2000, block_size=200, temperature=0.9):
     """
     Args: 
@@ -21,19 +18,34 @@ def generateTXTconcat(model, tokenizer, prompt, num_words=2000, block_size=200, 
     Returns: 
         str: text generated
     """
+    local_path = "models"
+
+    tokenizer = GPT2Tokenizer.from_pretrained(local_path, local_files_only=True)
+    model = GPT2LMHeadModel.from_pretrained(local_path, local_files_only=True)
+
+    tokenizer.pad_token = tokenizer.eos_token
+    
     generated_text = prompt
     current_words = len(generated_text)
 
     # use the gpu if avaliable
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
+    #model = model.to(device)
+    max_context = 300
 
     while current_words < num_words:
-        input_ids = tokenizer.encode(generated_text, return_tensors="pt", truncation=True).to(device)
+        #input_ids = tokenizer.encode(generated_text, return_tensors="pt", truncation=True).to(device)
+        input_ids = tokenizer("Había una vez un niño llamado ", 
+                              return_tensors="pt", 
+                              padding=True, 
+                              truncation=True,
+                              max_length=max_context
+                              )
 
         output_ids = model.generate(
-            input_ids,
-            max_length=input_ids.shape[1] + block_size * 2,  # más tokens que palabras
+            input_ids["input_ids"],
+            max_length=input_ids["input_ids"].shape[1] + block_size * 2,  # más tokens que palabras
+            attention_mask=input_ids["attention_mask"],
             do_sample=True,
             top_k=50,
             top_p=0.9,
@@ -76,6 +88,9 @@ def savePDF(text, filename="output.pdf"):
 
 if __name__ == "__main__":
     # generate the text
+    local_path = "models"
+    tokenizer = GPT2Tokenizer.from_pretrained(local_path, local_files_only=True)
+    model = GPT2LMHeadModel.from_pretrained(local_path, local_files_only=True)
     text = generateTXTconcat(
         model, tokenizer,
         prompt="Había una vez ",
@@ -84,4 +99,4 @@ if __name__ == "__main__":
     )
 
     # save the text in a pdf file
-    savePDF(text=text, "example.pdf")
+    savePDF(text=text, filename="example.pdf")
